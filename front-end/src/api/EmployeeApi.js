59c9516ffd3ab8bcdcb9a7ab9ea037c7c556d5b8
chaovincent7 <117843673+chaovincent7@users.noreply.github.com>
2022-12-13 13:24:48 -0600

    Created EmployeeApi

---
+++ front-end/src/api/EmployeeApi.js
@@ -0,0 +1,107 @@
+// const BASE = "http://localhost:8080"  // use this if running locally
+const BASE = "http://0.0.0.0:8080" // edit this with your AWS endpoint
+const URI = BASE + "/api"
+
+const EmployeeApi = {
+    
+    getAll: (setEmployeeList) => {
+
+        fetch(URI + "/employee")
+            .then( result => result.json() )
+            .then( data => {
+                setEmployeeList(data)
+            } )
+            .catch( error => { console.log(error) } )
+    },
+
+    add: (employee) => {
+        
+        fetch(URI + "/add/employee", {
+            method: "POST",
+            body: JSON.stringify(employee),
+            headers: { "Content-Type": "application/json" }
+        })
+            .then( result => result.json() )
+            .then( data => {
+
+                if(typeof data.id !== 'undefined') {
+
+                    console.log("CREATED EMPLOYEE:");
+                    console.log(data);
+
+                    alert(`EMPLOYEE CREATED \n` +
+                        `------------------------\n` + 
+                        `ID: ${data.id}\n` +
+                        `First Name: ${data.firstName}\n` +
+                        `Last Name: ${data.lastName}\n` +
+                        `Email: ${data.email}\n` +
+                        `Phone: ${data.phone}\n`
+                    )
+                }
+                else {
+                    alert("Employee can't be created, check that you are not using an email already in use by another employee.")
+                }
+
+            } )
+            .catch( error => { 
+                console.log(error);
+            } )
+
+    },
+
+    update: (employee, employeeList, setEmployeeList) => {
+
+        fetch(URI + "/update/employee",  {
+            method: 'PUT',
+            body: JSON.stringify(employee),
+            headers: {  "Content-Type": "application/json" }
+        })
+            .then( result => result.json() )
+            .then( data => {
+
+                if(typeof data.id !== 'undefined') {
+
+                    console.log("UPDATED:");
+                    console.log(data);
+
+                    const newList = [...employeeList];
+
+                    let index = -1;
+
+                    for(let i = 0; i < newList.length; i++) {
+
+                        if( newList[i].id === data.id ) {
+                            index = i;
+                            break;
+                        }
+                    }
+
+                    newList.splice(index, 1, data)
+
+                    setEmployeeList(newList)
+                }
+                else {
+                    alert("Error updating employee, email choosen may already be in use by another employee")
+                }
+                
+            } )
+            .catch(error => { console.log(error); })
+
+    },
+
+    delete: (id) => {
+
+        fetch(URI + "/delete/employee/" + id, {
+            method: "DELETE"
+        })
+            .then( result => result.json() )
+            .then( data => {
+                console.log("DELETED:");
+                console.log(data);
+            } )
+            .catch(error => { console.log(error); })
+
+    }
+}
+
+export default EmployeeApi;
---
+++ front-end/src/components/signedInPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const signedInPage = () => {
+    return(
+        <div>
+            
+        </div>
+    )
+}
+
+
+export default signedInPage
