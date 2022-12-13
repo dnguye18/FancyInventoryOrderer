38edc5902e4055637251dc136183ef96dc4a96cc
chaovincent7 <117843673+chaovincent7@users.noreply.github.com>
2022-12-12 19:38:13 -0600

    Added Inventory/Employee Files and 90% done on Login Page

---
+++ front-end/src/components/landingpage.js
@@ -0,0 +1,33 @@
+import React from "react";
+import '../css/landingpage.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const LandingPage = () => {
+    return(
+        <body>
+            <div className="container">
+                <p className="welcome">Welcome</p>
+                <img src="https://www.hanaretail.com/wp-content/uploads/2022/03/2-2.png" alt="loginPic"/>
+                <div className="loginContainer">
+                    <br></br>
+                    <input type="text" class="form__field" placeholder="Username" name="Username" id='Username' required />
+                    
+                    <br></br>
+                    <input type="password" class="form__field" placeholder="Password" name="Password" id='Password' required />
+
+                    <p></p>
+
+                    <button className="signUp">
+                        Sign in
+                    </button>
+
+                    <p>Dont have an account?</p>
+
+                    <button className="create">Create Account</button>
+                </div>
+            </div>
+        </body>
+    )
+}
+
+export default LandingPage
