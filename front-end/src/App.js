3e3c4fcfb238091a2f8be8866d4cfc533daef47b
Jacob Cantrell <jcantrell218@gmail.com>
2022-12-15 11:25:02 -0500

    updated EmployeeAPI and landingpage.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,15 +3,33 @@ import './App.css';
 import CreateAccount from './components/CreateAccount';
 import LandingPage from './components/landingpage';
 import SignedInPage from './components/signedInPage';
+import InventoryView from './components/Inventory/InventoryView';
+import InventoryAdd from './components/Inventory/InventoryAdd';
+import InventoryUpdate from './components/Inventory/InventoryUpdate';
+import InventoryDelete from './components/Inventory/InventoryDelete';
+
+import EmployeeView from './components/Employee/EmployeeView';
+import EmployeeAdd from './components/Employee/EmployeeAdd';
+import EmployeeDelete from './components/Employee/EmployeeDelete';
+import EmployeeUpdate from './components/Employee/EmployeeUpdate';
 
 function App() {
   return (
     <div className="Appcontainer">
-      
       <Routes>
-        <Route path='/' element={<LandingPage />} />
+        <Route path='/' element={<LandingPage />} exact/>
         <Route path="/create_profile" element={<CreateAccount />} />
-        <Route path='/logged_in' element={<SignedInPage />} />
+        <Route path='/logged_in/*' element={<SignedInPage />} />
+
+        <Route path='/inventory/view' element={<InventoryView />} />
+        <Route path='/inventory/add' element={<InventoryAdd />} />
+        <Route path='/inventory/delete' element={<InventoryDelete />} />
+        <Route path='/inventory/update' element={<InventoryUpdate />} />
+
+        <Route path='/employee/view' element={<EmployeeView />} />
+        <Route path='/employee/add' element={<EmployeeAdd />} />
+        <Route path='/employee/delete' element={<EmployeeDelete />} />
+        <Route path='/employee/update' element={<EmployeeUpdate />} />
       </Routes>
     </div>
   );
